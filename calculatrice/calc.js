859de212f7101627e552d957786c4090b12cb8cf
simplon <costanzo.isabel@gmail.com>
2017-07-04 15:10:04 +0200

    function n1

--- calculatrice/calc.js
+++ calculatrice/calc.js
@@ -1,9 +1,38 @@
-var monArmoire= 4 ;
-monArmoire=Number(prompt(2));
-alert( monArmoire +2);
 
-var monPote=2;
-monPote=prompt(7);
-alert( monPote  *7)
+var calcul=0;
+var calcul2=0;
+
+
+
+ calcul=Number(prompt("entrez le nombre"));
+ calcul2=Number(prompt("nombre à ajouter"));
+
+function addition(isa1,isa2) 
+{
+
+var result;
+result=isa1+isa2;
+return result; 
+} 
+
+
+
+
+var resultAddition=addition(calcul,calcul2);
+alert(resultAddition);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

92b11747351bac460c938ffc512f8e12a727566f
simplon <costanzo.isabel@gmail.com>
2017-07-04 15:49:32 +0200

    solution a trouver

--- calculatrice/calc.js
+++ calculatrice/calc.js
@@ -1,10 +1,7 @@
 
 var calcul=0;
 var calcul2=0;
-
-
-
- calcul=Number(prompt("entrez le nombre"));
+calcul=Number(prompt("entrez le nombre"));
  calcul2=Number(prompt("nombre à ajouter"));
 
 function addition(isa1,isa2) 
@@ -14,14 +11,18 @@ var result;
 result=isa1+isa2;
 return result; 
 } 
-
-
-
-
 var resultAddition=addition(calcul,calcul2);
 alert(resultAddition);
 
 
+function multi(pepette1,pepette2){
+
+var result;
+result=pepette1*pepette2;
+return result;
+}
+ var resultMultiplication=multi(calcul,calcul2);
+ alert(resultMultiplication);
 
 
 
